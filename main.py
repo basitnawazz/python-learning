f5e6594f1e9d3ff9f660b456615beca98d3ffb2c
Basit Nawaz <127959828+basitnawazz@users.noreply.github.com>
2023-03-16 14:41:49 +0000

    Comments, Escape Seqences &

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-print("basitnawaz")
-print(7)
-print("blue")
-print(234*34522)
-print("my name is good")
-print("what are you doing ")
-print("good")
+print("hey i am a good boy")
+#hey basit nawaz 
+#Author:Basit 
 
-print (9+5)
 
-print ("this for git hub")
-print ("this is for Testing Branch")
+print ("hello world")
+print ("i am a good\'boy\'but i am bad boy also")
+print("this is print is a statment.")
+print ("hello basit\and doremone") #printing Hello basit\and doremone 
+
+#Wow so good code 
+print ("we are doing 100 days coding")
+print ("Hey", 7, 8, sep="~" , end="2007")
+print ("basit nawaz")
+
